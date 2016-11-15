4afc7b6e126d44a54a4c92d85402eeac160ee973
Paul Colomiets <paul@colomiets.name>
2016-11-15 02:17:38 +0200

    Example of service without a service trait

---
+++ examples/new_service.rs
@@ -0,0 +1,73 @@
+extern crate futures;
+extern crate tokio_core;
+extern crate tk_bufstream;
+
+use std::io;
+use std::str;
+use std::io::Write;
+use std::net::SocketAddr;
+use std::time::Duration;
+use std::env;
+
+use futures::{Future, Sink};
+use futures::stream::Stream;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::{Core, Timeout};
+use tk_bufstream::{Buf, IoBuf, Encode, Decode};
+
+const MAX_CONNECTIONS: usize = 200;
+
+struct Codec;
+
+impl Encode for Codec {
+    type Item = String;
+    fn encode(&mut self, data: String, buf: &mut Buf) {
+        writeln!(buf, "{}", &data).unwrap();
+    }
+}
+
+impl Decode for Codec {
+    type Item = String;
+    fn decode(&mut self, buf: &mut Buf) -> Result<Option<String>, io::Error> {
+        if let Some(end) = buf[..].iter().position(|&x| x == b'\n') {
+            let s = str::from_utf8(&buf[..end])
+                .map(|v| String::from(v))
+                .map_err(|_| io::Error::new(io::ErrorKind::Other,
+                                            "can't decode utf-8"))?;
+            buf.consume(end+1);
+            Ok(Some(s))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+fn main() {
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:7777".to_string());
+    let addr = addr.parse::<SocketAddr>().unwrap();
+
+    let mut lp = Core::new().unwrap();
+    let handle = lp.handle();
+    let socket = TcpListener::bind(&addr, &handle).unwrap();
+    println!("Listening on: {}", addr);
+
+    let done = socket.incoming()
+        .map_err(|e| { println!("Accept error: {}", e); })
+        .map(|(socket, _addr)| {
+            let (stream, sink) = IoBuf::new(socket)
+                .framed(Codec).split();
+            sink.send_all(
+                stream
+                .map(|l| {
+                    Timeout::new(Duration::new(5, 0), &handle).unwrap()
+                    .map(move |()| format!("after timeout: {}", l))
+                })
+                .buffered(5)
+            )
+            .map(|_| ())
+            .map_err(|e| { println!("Connection error: {}", e); })
+        }).buffer_unordered(MAX_CONNECTIONS)
+          .for_each(|()| Ok(()));
+    lp.run(done).unwrap();
+}
+
